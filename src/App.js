645f3ea5eca12e6376dc9113a3e2d9df2e9d6e11
jpswt <joel_swt@yahoo.com>
2022-10-25 10:11:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { DnDProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import DragDrop from './components/DragDrop';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<DnDProvider backend={HTML5Backend}>
+			<div className="app">
+				<DragDrop />
+			</div>
+		</DnDProvider>
+	);
 }
 
 export default App;
---
+++ src/components/DragDrop.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const pics = [
+	{
+		id: 1,
+		url: '',
+	},
+];
+
+function DragDrop() {
+	return (
+		<>
+			<div className="pictures"></div>
+			<div className="board"></div>
+		</>
+	);
+}
+
+export default DragDrop;
